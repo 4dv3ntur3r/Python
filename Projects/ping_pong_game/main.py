7a8524e7ab95b6a59d52e4a9540a57460857f98f
Lasantha chinthaka <chintha987@gmail.com>
2022-03-19 10:40:20 -0400

    Developed a ping pong game
    Developed a ping pong multiplayer game. Game uses the key bindings of up and down arrow keys and small w and smalls for player 2. If clicked small q the program will close it self

---
+++ Projects/ping_pong_game/main.py
@@ -0,0 +1,55 @@
+import turtle
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+player1 = 0
+player2 = 0
+
+is_game_on = True
+
+
+def close_game():
+    global is_game_on
+    is_game_on = False
+
+
+while is_game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_position()
+
+    screen.onkey(close_game, "q")
+
